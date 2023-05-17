6c43d1cb1a1bb5d423732feaca112f257cbefb37
valexx55 <valexx55@gmail.com>
2023-05-17 20:11:42 +0200

    primeras anotaciones controller

--- imcwebprofe/src/main/java/edu/cas/imcweb/repository/entity/Paciente.java
+++ imcwebprofe/src/main/java/edu/cas/imcweb/repository/entity/Paciente.java
@@ -12,6 +12,39 @@ public class Paciente {
 	private String nombre;
 	private int edad;
 	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public Paciente(int id, String nombre, int edad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	public Paciente() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "Paciente [id=" + id + ", nombre=" + nombre + ", edad=" + edad + "]";
+	}
+	
 	
 
 }
--- imcwebprofe/src/main/java/edu/cas/imcwebprofe/controller/PacienteController.java
+++ imcwebprofe/src/main/java/edu/cas/imcwebprofe/controller/PacienteController.java
@@ -1,18 +1,65 @@
 package edu.cas.imcwebprofe.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.cas.imcweb.repository.entity.Paciente;
 
 //ESTA ES LA CAPA "SERVLETS"
 //AQUÍ HAREMOS LOS MÉTODOS PARA
-//ALTA
-//BAJA
-//MODIFICACIÓN
-//CONUSLTA DE UN PACIENTE
+//ALTA POST
+//BAJA DELETE
+//MODIFICACIÓN PUT
+//CONUSLTA DE UN PACIENTE GET
+//CONUSLTA DE TODOS LOS PANCIENTES GET
 //ESTO VA LIGADO A LA DEPDECNIA DE HTTP
 
 //@Controller //le digo que esta clase, envía y recibe HTTP CON JSONES EN EL CUERPO
-@RestController //con esto, digo que lo que envío y recibo es el tipo MIME JSON
+@RestController // con esto, digo que lo que envío y recibo es el tipo MIME JSON
+@RequestMapping("/paciente") // oye, lo que sea http://localhost:8081/paciente es para esta clase!!
 public class PacienteController {
 
+	@GetMapping("/obtener-paciente-test") // GET http://localhost:8081/paciente/obtener-paciente-test
+	public Paciente obtenerPacienteTest() {
+		Paciente paciente = null;
+
+		paciente = new Paciente(5, "Sheila", 48);
+
+		return paciente;
+	}
+	
+	//http://localhost:8081/paciente/obtenerpaciente?idpaciente=3
+	
+	@GetMapping("/obtenerpaciente") // GET http://localhost:8081/paciente/obtenerpaciente
+	public Paciente obtenerPacienteIdParam(@RequestParam(name = "idpaciente")  Integer idpaciente) {
+		Paciente paciente = null;
+
+		System.out.println("ID Paciente recibido = " + idpaciente);
+		paciente = new Paciente(5, "Sheila", 48);
+
+		return paciente;
+	}
+
+	@GetMapping("/obtenerpaciente/{id}") // GET http://localhost:8081/paciente/obtenerpaciente/5
+	public Paciente obtenerPacienteIdPath(@PathVariable Integer id) {
+		Paciente paciente = null;
+
+		System.out.println("ID Paciente recibido = " + id);
+		paciente = new Paciente(5, "Sheila", 48);
+
+		return paciente;
+	}
+	
+	@PostMapping // POST http://localhost:8081/paciente
+	public Paciente insertarPaciente(@RequestBody Paciente paciente) {
+		System.out.println("Post paciente " + paciente);
+		paciente.setEdad(paciente.getEdad() + 1);
+		return paciente;
+
+	}
 }
